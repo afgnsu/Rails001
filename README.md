db49d63a64fd7e542be185ca0d7deeff03b8d3a7
Patrick Su <afgnsu@gmail.com>
2023-01-12 08:08:34 +0000

    Cool

---
+++ README.md
@@ -0,0 +1,36 @@
+# README
+
+This Demo is showing the use of Ajax Datatables Rails.
+It is accompanied with the article I wrote in [Medum][MED] introducing the use of RubyGems.
+
+
+[MED]: <https://medium.com/實用的rubygems/ajax-datatables-rails-功能齊全的資料列表-98c96e9a250a>
+
+[SOURCE]: <https://github.com/jbox-web/ajax-datatables-rails.git>
+
+## 安裝
+
+01. git clone https://github.com/jbox-web/ajax-datatables-rails.git
+
+02. rvm install 2.6.6
+
+03. ruby -v  # 2.6.6
+
+04. rvm gemset create rails617
+
+05. rvm use 2.6.6@rails617
+
+06. gem install rails -v 6.1.7
+
+07. cd demoajax
+
+08. bundle
+
+09. vi config/database.yml ，新增 username: 和 password: 值
+
+10. rake db:setup
+
+11. rails s -b 0.0.0.0
+
+`註：經我修改，把 *.haml 改成 *.erb，增加可讀性。`
+
---
+++ app/datatables/user_datatable.rb
@@ -0,0 +1,51 @@
+class UserDatatable < AjaxDatatablesRails::ActiveRecord
+
+  def view_columns
+    # Declare strings in this format: ModelName.column_name
+    # or in aliased_join_table.column_name format
+    @view_columns ||= {
+      # id: { source: "User.id", cond: :eq },
+      # name: { source: "User.name", cond: :like }
+      name: { source: "User.name", cond: :like },
+      age: { source: "User.age", cond: :eq },
+      email: { source: "User.email", cond: :like },
+      company: { source: "User.company", cond: :like },
+      job: { source: "User.job", cond: :like }
+    }
+  end
+
+  def data
+    records.map do |record|
+      u = record
+      {
+        "name": u.name,
+        "age": u.age,
+        "email": u.email,
+        "company": u.company,
+        "job": u.job,
+      }
+    end
+  end
+
+  private
+
+  def get_raw_records
+    # insert query here
+    #User.all.order(age: :desc, name: :desc)
+    User.all
+  end
+
+  # ==== These methods represent the basic operations to perform on records
+  # and feel free to override them
+
+  # def filter_records(records)
+  # end
+
+  # def sort_records(records)
+  # end
+
+  # def paginate_records(records)
+  # end
+
+  # ==== Insert 'presenter'-like methods below if necessary
+end
